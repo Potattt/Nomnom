c3c6fa18b66c0852fae6545b75f2ea536975f903
undulaattim <yumalainen@gmail.com>
2026-03-17 13:28:11 +0200

    wdada

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int maxHealth = 5;
+    private int currentHealth;
+
+    [Header("Sprites (Assign in Inspector)")]
+    public Sprite[] burgerSprites; // drag your burger stages here
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        UpdateBurgerVisual();
+    }
+
+    void Update()
+    {
+        // TEST: press Space to take damage
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        UpdateBurgerVisual();
+
+        if (currentHealth == 0)
+        {
+            Debug.Log("Burger fully eaten!");
+        }
+    }
+
+    void UpdateBurgerVisual()
+    {
+        int index = maxHealth - currentHealth;
+
+        if (index >= burgerSprites.Length)
+            index = burgerSprites.Length - 1;
+
+        spriteRenderer.sprite = burgerSprites[index];
+    }
+}
